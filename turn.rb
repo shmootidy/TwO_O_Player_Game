bea2e464cc57265d67ae1cb6cf4512cba0faa97c
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-19 01:42:08 +0000

    Retrieve result of good_response (Question) in Turn initialize.

--- turn.rb
+++ turn.rb
@@ -1,9 +1,10 @@
 class Turn
-  attr_reader :current_player
+  attr_reader :current_player, :good_response
 
   def initialize
     @current_player = 1
     question = Question.new
+    @good_response = question.good_response
     switch_player
   end
 
