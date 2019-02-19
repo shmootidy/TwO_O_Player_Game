88abd89e3af9047c3d89ab19ac467a837542fdb9
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-19 00:42:16 +0000

    Move require into main, out of game. Whoops.

--- game.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'main'
-require_relative 'player'
-require_relative 'question'
-require_relative 'turn'
-
---
+++ main.rb
@@ -0,0 +1,5 @@
+require_relative 'main'
+require_relative 'player'
+require_relative 'question'
+require_relative 'turn'
+
