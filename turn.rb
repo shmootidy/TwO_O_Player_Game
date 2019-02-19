c3fa4443340dbd1134dda9a2e93ade9459da5f8b
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-19 01:37:50 +0000

    Add switch player method to Turn class.

--- turn.rb
+++ turn.rb
@@ -4,6 +4,15 @@ class Turn
   def initialize
     @current_player = 1
     question = Question.new
+    switch_player
+  end
+
+  def switch_player
+    if @current_player == 1
+      @current_player = 2
+    else
+      @current_player =1
+    end
   end
 
 end
