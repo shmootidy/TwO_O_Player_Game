fd4f2e25593ef62f225cc78e1ade9a82105317f9
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-19 00:47:28 +0000

    Begin writing Player class, with initialize, lose_life, and attr_reader methods.

---
+++ player.rb
@@ -0,0 +1,14 @@
+class Player
+  attr_reader :lives
+
+  def initialize
+    @lives = 3
+  end
+
+  def lose_life
+    @lives -= 1
+  end
+
+end
+
+# two instances of this class will be created: player1 and player2
